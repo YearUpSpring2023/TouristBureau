0c21d249a88155300a33afe2a3e88a49cb9d4800
mattchristenson <matt@rcc.team>
2023-05-16 13:38:19 -0500

    Completed with some student enhancements
    Co-authored-by: ZionDanielson <ZionDanielson@users.noreply.github.com>
    Co-authored-by:  Marco Sanchez <Notmarbo@users.noreply.github.com>
    Co-authored-by: Kirdna <Kirdna@users.noreply.github.com>
    Co-authored-by: malic <imalicx@users.noreply.github.com>
    Co-authored-by: CosmicChris0606 <CosmicChris0606@users.noreply.github.com>
    Co-authored-by: alexawill23 <alexawill23@users.noreply.github.com>
    Co-authored-by: Michaeljean3456 <Michaeljean3456@users.noreply.github.com>
    Co-authored-by: cind6 <cind6@users.noreply.github.com>
    Co-authored-by: Jamys Roblero <jamjamandtoast@users.noreply.github.com>
    Co-authored-by: zKevin19 <zKevin19@users.noreply.github.com>

--- scripts/activities.js
+++ scripts/activities.js
@@ -31,17 +31,17 @@ window.onload = function () {
     hideActivitySelect();
     hideActivityDetail();
 
-};  
+};
 
 
-function onCategorySelectChange(){
+function onCategorySelectChange() {
     let currentCategory = categorySelect.value;
 
-    if(currentCategory == ""){
+    if (currentCategory == "") {
         hideActivitySelect();
- 
+
     }
-    else{
+    else {
         console.log(`The ${currentCategory} value has been selected for Category.`)
 
         //populate Activity List with only activities that belong to this Category.
@@ -49,11 +49,11 @@ function onCategorySelectChange(){
 
         //Display the Activity Selection area so that an activity can be selected.
         showActivitySelect();
-  
+
     }
 
     hideActivityDetail();
-    
+
 
 
 }
@@ -65,43 +65,80 @@ function onCategorySelectChange(){
 // then loop through those activities and list them.
 
 
-function populateActivitySelect(fullListOfActivities, selectedCategory){
+function getActivitiesInCategory(fullListOfActivities, category) {
+    let result = [];
+
+    //This is the solution using an index (i)
+
+    // for (let i = 0; i < fullListOfActivities.length; i++){
+    //     if(fullListOfActivities[i].category == category){
+    //         result.push(fullListOfActivities[i])
+    //     } 
+    // }
+
+
+    // for (let i = 0; i < fullListOfActivities.length; i++){
+    //     let thisActivity = fullListOfActivities[i];
+    //     if(thisActivity.category == category){
+    //         result.push(thisActivity)
+    //     } 
+    // }
+
+    //This is the solution without using an index.
+
+    for (let thisActivity of fullListOfActivities) {
+        if (thisActivity.category == category) {
+            result.push(thisActivity)
+        }
+    }
+
+    // Return the result that now contains each of the values that we identified as a match.
+    return result;
+}
+
+function populateActivitySelect(fullListOfActivities, selectedCategory) {
     // This function should populate the second dropdown with only the 
     // activities that exist in the selected category.
-
-    //activitySelect.options.length = 0;
 
     activitySelect.innerHTML = "";
 
     let initialOption = new Option("Please select your activity!", "");
     activitySelect.appendChild(initialOption);
 
-    for( let i = 0 ; i < fullListOfActivities.length ; i++ ) {
-        let thisActivity = fullListOfActivities[i];
+    let activitiesIsCategory = getActivitiesInCategory(fullListOfActivities, selectedCategory);
 
-        if(thisActivity.category == selectedCategory){
-           
-            let theOption = new Option(thisActivity.name, thisActivity.id );
-            activitySelect.appendChild(theOption);
-            console.log("The new option has been added to the dropdown")
-        }
-
+    for (let thisActivity of activitiesIsCategory) {
+        let theOption = new Option(thisActivity.name, thisActivity.id);
+        activitySelect.appendChild(theOption);
+        console.log("The new option has been added to the dropdown")
     }
 
 }
 
-function getActivitiesInCategory(fullListOfActivities, category){
-    let result = []
 
-    // todo to the work here to build out result with only the activities that are of the correct category:
+function populateActivitySelectOld(fullListOfActivities, selectedCategory) {
+    // This function should populate the second dropdown with only the 
+    // activities that exist in the selected category.
 
+    activitySelect.innerHTML = "";
 
+    let initialOption = new Option("Please select your activity!", "");
+    activitySelect.appendChild(initialOption);
 
-    return result;
+    for (let thisActivity of fullListOfActivities) {
+        if (thisActivity.category == selectedCategory) {
+
+            let theOption = new Option(thisActivity.name, thisActivity.id);
+            activitySelect.appendChild(theOption);
+            console.log("The new option has been added to the dropdown")
+        }
+    }
+
 }
 
 
-function onActivitySelectChange(){
+
+function onActivitySelectChange() {
     // populate all of the fields in the activity detail area with values that pertain
     // to the selected activity
 
@@ -109,12 +146,12 @@ function onActivitySelectChange(){
 
     let selectedActivityId = activitySelect.value;
 
-    if(selectedActivityId == ""){
+    if (selectedActivityId == "") {
         hideActivityDetail();
 
     }
-    else{
-        
+    else {
+
         let selectedActivity = getActivityById(selectedActivityId);
 
         activityName.innerHTML = selectedActivity.name;
@@ -128,22 +165,22 @@ function onActivitySelectChange(){
         // Display the activity detail row
         showActivityDetail();
 
-        if(selectedActivity.price > 0){
+        if (selectedActivity.price > 0) {
             showCheckout();
         }
-        else{
+        else {
             hideCheckout();
         }
 
     }
-    
+
 }
 
-function getActivityById(id){
-    for( let i = 0 ; i < activities.length ; i++){
+function getActivityById(id) {
+    for (let i = 0; i < activities.length; i++) {
         let thisActivity = activities[i];
-        if(thisActivity.id == id){
-            return thisActivity;            
+        if (thisActivity.id == id) {
+            return thisActivity;
         }
 
     }
