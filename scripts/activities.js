906a8ae5c026115c41e6df24a3146499d83935f7
mattchristenson <matt@rcc.team>
2023-05-16 09:49:14 -0500

    Finsiehd Javascript in class!
    Co-authored-by: ZionDanielson <ZionDanielson@users.noreply.github.com>
    Co-authored-by:  Marco Sanchez <Notmarbo@users.noreply.github.com>
    Co-authored-by: Kirdna <Kirdna@users.noreply.github.com>
    Co-authored-by: malic <imalicx@users.noreply.github.com>
    Co-authored-by: CosmicChris0606 <CosmicChris0606@users.noreply.github.com>
    Co-authored-by: alexawill23 <alexawill23@users.noreply.github.com>
    Co-authored-by: Michaeljean3456 <Michaeljean3456@users.noreply.github.com>
    Co-authored-by: cind6 <cind6@users.noreply.github.com>
    Co-authored-by: Jamys Roblero <jamjamandtoast@users.noreply.github.com>
    Co-authored-by: zKevin19 <zKevin19@users.noreply.github.com>

--- scripts/activities.js
+++ scripts/activities.js
@@ -17,6 +17,12 @@ const activitySelectionRow = document.getElementById("activitySelectionRow");
 const categorySelect = document.getElementById("categorySelect");
 const activitySelect = document.getElementById("activitySelect");
 
+const activityName = document.getElementById("activityName");
+const activityId = document.getElementById("activityId");
+const activityDesc = document.getElementById("activityDesc");
+const activityLocation = document.getElementById("activityLocation");
+const activityPrice = document.getElementById("activityPrice");
+
 window.onload = function () {
 
     categorySelect.onchange = onCategorySelectChange;
@@ -25,41 +31,103 @@ window.onload = function () {
     hideActivitySelect();
     hideActivityDetail();
     hideCheckout();
-};
+};  
 
 
 function onCategorySelectChange(){
     let currentCategory = categorySelect.value;
-    console.log(`The ${currentCategory} value has been selected for Category.`)
 
-    //populate Activity List with only activities that belong to this Category.
-    populateActivitySelect(currentCategory);
+    if(currentCategory == ""){
+        hideActivitySelect();
+ 
+    }
+    else{
+        console.log(`The ${currentCategory} value has been selected for Category.`)
 
-    //Display the Activity Selection area so that an activity can be selected.
-    showActivitySelect();
+        //populate Activity List with only activities that belong to this Category.
+        populateActivitySelect(activities, currentCategory);
+
+        //Display the Activity Selection area so that an activity can be selected.
+        showActivitySelect();
+  
+    }
+
+    hideActivityDetail();
+    hideCheckout();      
+
 
 }
 
 
-function populateActivitySelect(selectedCategory){
+function populateActivitySelect(fullListOfActivities, selectedCategory){
     // This function should populate the second dropdown with only the 
     // activities that exist in the selected category.
 
-    // todo...
+    activitySelect.options.length = 0;
+
+    let initialOption = new Option("Please select your activity!", "");
+    activitySelect.appendChild(initialOption);
+
+    for( let i = 0 ; i < fullListOfActivities.length ; i++ ) {
+        let thisActivity = fullListOfActivities[i];
+
+        if(thisActivity.category == selectedCategory){
+           
+            let theOption = new Option(thisActivity.name, thisActivity.id );
+            activitySelect.appendChild(theOption);
+            console.log("The new option has been added to the dropdown")
+        }
+
+    }
+
 }
+
 
 function onActivitySelectChange(){
     // populate all of the fields in the activity detail area with values that pertain
     // to the selected activity
 
-    // todo ...
+    // get the id of the selected activity:
 
-    // Display the activity detail row
-    showActivityDetail();
+    let selectedActivityId = activitySelect.value;
 
-    // IF (the cost of this activity is > 0 )
-    // THEN showcheckout()
-    // otherwise hideCheckout()
+    if(selectedActivityId == ""){
+        hideActivityDetail();
+    }
+    else{
+        
+        let selectedActivity = getActivityById(selectedActivityId);
+
+        activityName.innerHTML = selectedActivity.name;
+        activityId.innerHTML = selectedActivity.id;
+        activityDesc.innerHTML = selectedActivity.description;
+        activityLocation.innerHTML = selectedActivity.location;
+        activityPrice.innerHTML = selectedActivity.price;
+
+
+
+        // Display the activity detail row
+        showActivityDetail();
+
+        if(selectedActivity.price > 0){
+            showCheckout();
+        }
+        else{
+            hideCheckout();
+        }
+
+    }
+    
+}
+
+function getActivityById(id){
+    for( let i = 0 ; i < activities.length ; i++){
+        let thisActivity = activities[i];
+        if(thisActivity.id == id){
+            return thisActivity            
+        }
+
+    }
 }
 
 
