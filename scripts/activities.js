21b5b2002ae822d44f938d65cf6f2cc99e3a8f27
mattchristenson <matt@rcc.team>
2023-05-16 13:39:48 -0500

    Stripped of comments!!

--- scripts/activities.js
+++ scripts/activities.js
@@ -2,14 +2,6 @@
 
 console.log("Loading activities.js");
 
-// ESAMPLE ACTIVITY:
-// category: "Adventures", 
-// id: "A101", 
-// name: "Valley Hot Air Balloons", 
-// description: "Enjoy a lovely hot air balloon ride over the valley at sunrise.  Call 800-555-1212 to reserve a date/time after you complete your e-ticket purchase.", 
-// location: "121 S. Main Street",
-// price: 265.00 
-
 const activityDetailRow = document.getElementById("activityDetailRow");
 const paymentDetailRow = document.getElementById("paymentDetailRow");
 const activitySelectionRow = document.getElementById("activitySelectionRow");
@@ -33,58 +25,28 @@ window.onload = function () {
 
 };
 
-
 function onCategorySelectChange() {
     let currentCategory = categorySelect.value;
 
     if (currentCategory == "") {
         hideActivitySelect();
-
     }
     else {
         console.log(`The ${currentCategory} value has been selected for Category.`)
 
-        //populate Activity List with only activities that belong to this Category.
         populateActivitySelect(activities, currentCategory);
 
-        //Display the Activity Selection area so that an activity can be selected.
         showActivitySelect();
 
     }
 
     hideActivityDetail();
 
-
-
 }
-
-//ASSIGNMENT TODO:
-// Re-write the below function so that it uses a new 
-// function named getActivitiesInCategory (defined and created below)
-// to get an array with only the activities that need to be listed.
-// then loop through those activities and list them.
 
 
 function getActivitiesInCategory(fullListOfActivities, category) {
     let result = [];
-
-    //This is the solution using an index (i)
-
-    // for (let i = 0; i < fullListOfActivities.length; i++){
-    //     if(fullListOfActivities[i].category == category){
-    //         result.push(fullListOfActivities[i])
-    //     } 
-    // }
-
-
-    // for (let i = 0; i < fullListOfActivities.length; i++){
-    //     let thisActivity = fullListOfActivities[i];
-    //     if(thisActivity.category == category){
-    //         result.push(thisActivity)
-    //     } 
-    // }
-
-    //This is the solution without using an index.
 
     for (let thisActivity of fullListOfActivities) {
         if (thisActivity.category == category) {
@@ -92,13 +54,10 @@ function getActivitiesInCategory(fullListOfActivities, category) {
         }
     }
 
-    // Return the result that now contains each of the values that we identified as a match.
     return result;
 }
 
 function populateActivitySelect(fullListOfActivities, selectedCategory) {
-    // This function should populate the second dropdown with only the 
-    // activities that exist in the selected category.
 
     activitySelect.innerHTML = "";
 
@@ -117,8 +76,6 @@ function populateActivitySelect(fullListOfActivities, selectedCategory) {
 
 
 function populateActivitySelectOld(fullListOfActivities, selectedCategory) {
-    // This function should populate the second dropdown with only the 
-    // activities that exist in the selected category.
 
     activitySelect.innerHTML = "";
 
@@ -139,10 +96,6 @@ function populateActivitySelectOld(fullListOfActivities, selectedCategory) {
 
 
 function onActivitySelectChange() {
-    // populate all of the fields in the activity detail area with values that pertain
-    // to the selected activity
-
-    // get the id of the selected activity:
 
     let selectedActivityId = activitySelect.value;
 
@@ -160,9 +113,6 @@ function onActivitySelectChange() {
         activityLocation.innerHTML = selectedActivity.location;
         activityPrice.innerHTML = selectedActivity.price;
 
-
-
-        // Display the activity detail row
         showActivityDetail();
 
         if (selectedActivity.price > 0) {
@@ -182,7 +132,6 @@ function getActivityById(id) {
         if (thisActivity.id == id) {
             return thisActivity;
         }
-
     }
 }
 
